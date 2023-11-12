cd70f206a7e14f0994db0a20d02f974276e5469a
jyPark4321 <94616697+jyPark4321@users.noreply.github.com>
2023-11-12 22:18:55 +0900

    Update test.py

--- calculator/test.py
+++ calculator/test.py
@@ -1,1 +1,22 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from stack import Stack
+import calc
+
 print("연동 테스트파일입니다.")
+print("1.+,-,* 테스트를 위한 두개의 숫자 입력")
+
+
+a = int(input())
+b = int(input())
+c = calc.plus(a,b)
+d = calc.minus(a,b)
+f = calc.multi(a,b)
+print(c,d,f)
+
+
+test_data = list(input("스택을 이용해 연산 숫서 결정하기위한 함수 테스팅 입력 예) (2+3)*4:" ))
+infix = calc.Operands(test_data)
+postfix = Stack.InToPost(infix)
+print(Stack.InToPost(infix))
