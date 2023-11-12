2b047798ad23e74956f34bfcca831f6b06bed0eb
highcastle01 <76648806+highcastle01@users.noreply.github.com>
2023-11-12 13:59:16 +0900

    Create Stack.py

---
+++ stack/Stack.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, data):
+        node = Node(data)
+        if self.top is None:
+            self.top = node
+        else:
+            node.next = self.top
+            self.top = node
+
+    def pop(self):
+        if self.top is None:
+            return None
+        node = self.top
+        self.top = node.next
+        return node.data
+
+    def peek(self):
+        if self.top is None:
+            return None
+        return self.top.data
+
+    def is_empty(self):
+        return self.top is None
