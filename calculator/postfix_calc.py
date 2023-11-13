181629697f7fcf21c52e014afc9b630f2b62863f
highcastle01 <76648806+highcastle01@users.noreply.github.com>
2023-11-14 01:51:45 +0900

    Update postfix_calc.py

--- calculator/postfix_calc.py
+++ calculator/postfix_calc.py
@@ -1,19 +1,20 @@
-from Stack import Stack, priority
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from stack import Stack
+from calc import plus, minus, multi, priority
 
-priority = {'(': 1, ')': 1, '+': 2, '-': 2, '*': 3, '/': 3}
-
-def postfix_calc(postfix):
-    st = Stack()
+def Postfix_calc(postfix):
+    st = Stack.Stack()
     for cur in postfix:
         if cur in priority:
             operands1 = st.pop()
             operands2 = st.pop()
             if cur == '+':
-                st.push(plus(operands1 + operands2))
+                st.push(plus(operands1, operands2))
             elif cur == '-':
-                st.push(minus(operands1 + operands2))
+                st.push(minus(operands1, operands2))
             elif cur == '*':
-                st.push(mul(operands1 + operands2))
-            else:
-                st.push(cur)
+                st.push(multi(operands1, operands2))
+        else:
+            st.push(cur)
     return st.pop()
