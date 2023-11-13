deb6ccb30a028a327bd54c010faa7eac4dce872a
highcastle01 <76648806+highcastle01@users.noreply.github.com>
2023-11-14 01:46:00 +0900

    Update calc.py
    파라미터 a, b int형변환 추가

--- calculator/calc.py
+++ calculator/calc.py
@@ -1,11 +1,10 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from stack import stack
-
-
 def plus(a,b):
   try:
+    a = int(a)
+    b = int(b)
     result = a + b
     return result
   except:
@@ -14,6 +13,8 @@ def plus(a,b):
 
 def minus(a,b):
   try:
+    a = int(a)
+    b = int(b)
     result = a - b
     return result
   except:
@@ -22,12 +23,14 @@ def minus(a,b):
 
 def multi(a,b):
   try:
+    a = int(a)
+    b = int(b)
     result = a * b
     return result
   except:
     print(f"곱하기 에러 발생")
     return None
-#
+    
 def Operands(infix):
   numbers = list('0123456789.')
   recognized = []
@@ -47,30 +50,3 @@ def Operands(infix):
       recognized.append(infix[i])
       i += 1
   return recognized
-
-# def InToPost(infix):
-#   s = stack.Stack()
-#   postfix = []
-#   for i in infix:
-#     if i == '(':
-#       s.push(i)
-#     elif i == ')':
-#       while s.top() != '(':
-#         postfix.append(s.pop())
-#       s.pop()
-#     elif i in priority:
-#       while not s.isEmpty():
-#         if priority[s.top()] >= priority[i]:
-#           postfix.append(s.pop())
-#         else:
-#           break
-#       s.push(i)
-#     else:
-#       postfix.append(i)
-#   while not s.isEmpty():
-#     postfix.append(s.pop())
-#
-#   return postfix
-#
-###InToPost()기능 중 일부 기능 calc.py내에서 제대로 작동하지않아######
-###stack.py로 이동 후에 정상 작동함을 확인#####
